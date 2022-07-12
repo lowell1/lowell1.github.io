72d5c684a9d4e3d06733a36a2cb937abb295cf87
germ <29132034+lowell1@users.noreply.github.com>
2022-07-12 15:21:17 -0700

    add email link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 export default function Footer() {
+  function renderTooltip(props) {
+    console.log(props);
+    return (
+      <Tooltip id="button-tooltip" {...props}>
+        <span style={{ wordWrap: "normal" }}>lowelljacobs@mail.com</span>
+      </Tooltip>
+    );
+  }
+
   return (
-    <Container fluid>
-      <a href="https://www.linkedin.com/in/lowell-jacobs">
-        <i class="bi bi-linkedin"></i>
-      </a>
-      <a href="https://www.github.com/lowell1">
-        <i class="bi bi-github"></i>
-      </a>
-    </Container>
+    <>
+      <Container fluid>
+        <a href="https://www.linkedin.com/in/lowell-jacobs">
+          <i class="bi bi-linkedin"></i>
+        </a>
+        <a href="https://www.github.com/lowell1">
+          <i class="bi bi-github"></i>
+        </a>
+        <OverlayTrigger
+          placement="top"
+          delay={{ show: 250, hide: 40000 }}
+          overlay={renderTooltip}
+        >
+          <a href="mailto:lowelljacobs@mail.com">
+            <i class="bi bi-envelope"></i>
+          </a>
+        </OverlayTrigger>
+      </Container>
+    </>
   );
 }
